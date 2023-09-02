3b802e70902d24752288ee4e44bc0193a95dea44
dabruge <davidbrucegeddes@gmail.com>
2023-09-02 16:52:14 +0100

    hello_buttons.py: refactor text elements to variables

--- hello_buttons.py
+++ hello_buttons.py
@@ -5,7 +5,9 @@ class Window(tk.Tk):
         super().__init__()
         self.title("Hello Tkinter")
 
-        self.label = tk.Label(self, text="Choose one:")
+        self.label_text = tk.StringVar()
+        self.label_text.set("Choose one:")
+        self.label = tk.Label(self, textvar=self.label_text) # Must use textvar here instead of just text
         self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=30)
 
         hello_button = tk.Button(self, text="Say hello", command=self.say_hello)
@@ -15,10 +17,10 @@ class Window(tk.Tk):
         goodbye_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
     
     def say_hello(self):
-        self.label.configure(text="Hello World!")
+        self.label_text.set("Hello World!")
     
     def say_goodbye(self):
-        self.label.configure(text="Goodbye! \n (Closing in 2 seconds)")
+        self.label_text.set("Goodbye! \n (Closing in 2 seconds)")
         self.after(2000, self.destroy)
 
 if __name__ == "__main__":
